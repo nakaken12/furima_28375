049de72c364acd3d8c32d89a2948ba8bf9575fa7
nakaken12 <sypk2105@ezweb.ne.jp>
2020-08-05 16:42:44 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name_full_width, :first_name_full_width, :family_name_kana, :first_name_kana, :birthday])
+  end
+  
   private
 
   def basic_auth
