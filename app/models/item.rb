f21f6daf2f26c8542198df3e819738040abb9bdb
nakaken12 <sypk2105@ezweb.ne.jp>
2020-08-07 19:09:25 +0900

    バリデーション の不足箇所追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,8 @@ class Item < ApplicationRecord
   validates :name, :explanation, :category_id, :status_id, :shipping_charge_id, :shipping_origin_id, :days_until_shipping_id, presence: true
   with_options presence: true, format: { with: /[3-9][0-9]{2}|[1-9][0-9]{3,6}/, message: 'Price Out of setting rangre' } do
     validates :price
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :genre_id, numericality: { other_than: 1 } 
   end
 end
