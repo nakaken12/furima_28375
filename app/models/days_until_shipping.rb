4b7b60dc9518a0037de8ecea4fb75b6a24edc4ed
nakaken12 <sypk2105@ezweb.ne.jp>
2020-08-07 15:00:19 +0900

    ActiveHashモデルの編集

--- app/models/days_until_shipping.rb
+++ app/models/days_until_shipping.rb
@@ -1,2 +1,8 @@
-class DaysUntilShipping < ApplicationRecord
+class DaysUntilShipping < ActiveHash::Base
+  self.date = [
+    { id: 1, name: '---' },
+    { id: 2, name: '1~2日で発送' },
+    { id: 3, name: '3~4で発送' },
+    { id: 4, name: '5~7で発送' },
+  ]
 end
