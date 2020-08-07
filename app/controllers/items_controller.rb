c0dbc97fc087278b146783c5e67cc4471c504a9f
nakaken12 <sypk2105@ezweb.ne.jp>
2020-08-07 20:54:29 +0900

    バリデージョンの不足分箇所追加の修正とcreateアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,15 +9,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    redirect_to root_path
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :shipping_charge_id,
-                                :shipping_origin_id, :days_until_shipping_id, :price)
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :shipping_charge_id, :shipping_origin_id, :days_until_shipping_id, :price)
   end
 
 end
