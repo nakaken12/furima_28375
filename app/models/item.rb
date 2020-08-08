c9c14e273585f33482c0eb850c159ca98ca490e1
nakaken12 <sypk2105@ezweb.ne.jp>
2020-08-08 16:34:18 +0900

    ActiveHashの導入とmerge

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :shipping_origin
   belongs_to_active_hash :days_until_shipping
+  has_one_attached :image
+  validate :image_presence
+
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
 
   validates :name, :explanation, :category_id, :status_id, :shipping_charge_id, :shipping_origin_id, :days_until_shipping_id, presence: true
   with_options presence: true, format: { with: /[3-9][0-9]{2}|[1-9][0-9]{3,6}/, message: 'Price Out of setting rangre' } do
